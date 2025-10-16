b591e86e8d3bcd61e9d627d9897a303858894f97
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2025-10-16 16:12:52 +0300

    Add 'Settings' menu item to Device Config Backup

--- netbox_device_config/navigation.py
+++ netbox_device_config/navigation.py
@@ -5,7 +5,7 @@ menu = PluginMenu(
     groups=(
         ("Device Config", (
             PluginMenuItem(link='plugins:netbox_device_config:devicecredential_list', link_text='Credentials'),
-            PluginMenuItem(link='plugins:netbox_device_config:deviceconfighistory_list', link_text='Config History'),
+            PluginMenuItem(link='plugins:netbox_device_config:backup_settings_list', link_text='Settings'),
             PluginMenuItem(link="plugins:netbox_device_config:backup_statistics", link_text="Statistics"),
         )),
     ),
