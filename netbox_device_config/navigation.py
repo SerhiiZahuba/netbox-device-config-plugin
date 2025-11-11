c1b4b826867fbc01fef6e92a932181bd65342d0c
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2025-11-11 16:38:59 +0200

    Update navigation.py

--- netbox_device_config/navigation.py
+++ netbox_device_config/navigation.py
@@ -4,9 +4,10 @@ menu = PluginMenu(
     label="Device Config Backup",
     groups=(
         ("Device Config", (
-            PluginMenuItem(link='plugins:netbox_device_config:devicecredential_list', link_text='Credentials'),
-            PluginMenuItem(link='plugins:netbox_device_config:backup_settings_list', link_text='Settings'),
+            PluginMenuItem(link='plugins:netbox_device_config:devicecredential_list', link_text='Device List'),
+            PluginMenuItem(link='plugins:netbox_device_config:backup_templates_list', link_text='Templates'),
             PluginMenuItem(link="plugins:netbox_device_config:backup_statistics", link_text="Statistics"),
+           # PluginMenuItem(link="plugins:netbox_device_config:backup_settings, link_text='Settings'),
         )),
     ),
 )
