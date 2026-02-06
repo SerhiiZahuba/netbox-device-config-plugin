502473e062def31e3e854553ee7a208370852ff9
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2026-02-06 15:53:37 +0200

    Add Git settings menu item to navigation

--- netbox_device_config/navigation.py
+++ netbox_device_config/navigation.py
@@ -7,8 +7,10 @@ menu = PluginMenu(
             PluginMenuItem(link='plugins:netbox_device_config:devicecredential_list', link_text='Device List'),
             PluginMenuItem(link='plugins:netbox_device_config:backup_templates_list', link_text='Templates'),
             PluginMenuItem(link="plugins:netbox_device_config:backup_statistics", link_text="Statistics"),
-            PluginMenuItem(link='plugins:netbox_device_config:task_history', link_text='Task History'),
+         #   PluginMenuItem(link='plugins:netbox_device_config:task_history', link_text='Task History'),
             PluginMenuItem(link='plugins:netbox_device_config:config_search', link_text='Search Configs',),
+            PluginMenuItem(       link='plugins:netbox_device_config:git_settings',         link_text='Settings'       ),
+
         )),
     ),
 )
