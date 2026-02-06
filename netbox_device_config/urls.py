d18441b22fe4255a384af27c055476f04cb8a113
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2026-02-06 15:53:25 +0200

    Update urls.py

--- netbox_device_config/urls.py
+++ netbox_device_config/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('credentials/<int:pk>/edit/', views.DeviceCredentialEditView.as_view(), name='devicecredential_edit'),
     path('credentials/<int:pk>/test/', views.DeviceCredentialTestView.as_view(), name='devicecredential_test'),
     path('credentials/<int:pk>/backup/', views.DeviceCredentialBackupView.as_view(), name='devicecredential_backup'),
-    path('history/', views.DeviceConfigHistoryListView.as_view(), name='deviceconfighistory_list'),
+   # path('history/', views.DeviceConfigHistoryListView.as_view(), name='deviceconfighistory_list'),
     path('backup/<int:device_id>/', views.backup_device, name='backup_device'),
 
     path("config/<int:config_id>/", views.view_config, name="view_config"),
@@ -25,8 +25,8 @@ urlpatterns = [
     path('compare/<int:config_id>/', views.compare_config, name='compare_config'),
     path("statistics/", views.BackupStatisticsView.as_view(), name="backup_statistics"),
     path("view/<int:config_id>/", views.view_config, name="view_config"),
-    path("compare/<int:config_id>/", views.compare_config, name="compare_config"),
-    path("download/<int:config_id>/", views.download_config, name="download_config"),
+  ##  path("compare/<int:config_id>/", views.compare_config, name="compare_config"),
+  ##  path("download/<int:config_id>/", views.download_config, name="download_config"),
 
     path("templates/", views.BackupTemplatesListView.as_view(), name="backup_templates_list"),
     path("templates/add/", views.BackupTemplatesCreateView.as_view(), name="backup_templates_add"),
@@ -37,4 +37,13 @@ urlpatterns = [
     path("tasks/<int:pk>/", BackupTaskDetailView.as_view(), name="task_detail"),
     #search menu
     path("search/", views.ConfigSearchView.as_view(), name="config_search"),
+    # git
+    path("settings/git/", views.GitSettingsView.as_view(), name="git_settings"),
+
+
+    path("device/<int:device_id>/diff/", views.DeviceGitDiffView.as_view(), name="device_git_diff"),
+
+path("device/<int:device_id>/show/", views.DeviceGitShowView.as_view()),
+path("device/<int:device_id>/diff/", views.DeviceGitDiffView.as_view()),
+
 ]
