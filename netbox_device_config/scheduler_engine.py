bf9e3edd9f407e5ae355bb76b7f09cf04e1d453e
sergio_from_tauri <36307887+SerhiiZahuba@users.noreply.github.com>
2026-02-14 15:44:08 +0200

    Refactor task creation for backup credentials

--- netbox_device_config/scheduler_engine.py
+++ netbox_device_config/scheduler_engine.py
@@ -2,6 +2,8 @@ from django.utils import timezone
 from datetime import timedelta
 from .models import BackupSchedule, DeviceCredential, DeviceBackupTask
 from .tasks import run_backup_task
+from django.db.models import Q
+
 
 
 def should_run(schedule):
@@ -51,23 +53,34 @@ def run_scheduler():
         # -------------------------------------------------
         if s.device:
             creds = DeviceCredential.objects.filter(device=s.device)
+
+        elif s.virtual_machine:
+            creds = DeviceCredential.objects.filter(virtual_machine=s.virtual_machine)
+
         else:
             creds = DeviceCredential.objects.all()
+
+
+
 
         # -------------------------------------------------
         # CREATE TASK FOR EACH
         # -------------------------------------------------
         for cred in creds:
 
+            print("DEBUG cred id:", cred.id)
+            print("DEBUG device:", cred.device)
+            print("DEBUG vm:", cred.virtual_machine)
+
             task = DeviceBackupTask.objects.create(
-                device=cred.device,
-                virtual_machine=cred.virtual_machine,
+                device_id=cred.device_id,
+                virtual_machine_id=cred.virtual_machine_id,
                 credential=cred,
                 status="queued",
                 queued_at=timezone.now(),
             )
 
-            run_backup_task.delay(task.id)
+
 
         # -------------------------------------------------
         # LAST RUN
